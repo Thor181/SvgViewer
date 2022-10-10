342f0717869af4d86744f30664eb1c2b028f0111
TorgashinAA <torgashin.aa@yandex.ru>
2022-10-10 12:25:16 +0700

    fix

--- SvgViewer/DirectoriesWorker.cs
+++ SvgViewer/DirectoriesWorker.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SvgViewer
 {
@@ -20,20 +21,43 @@ namespace SvgViewer
 
         public static void WriteToFile(string path)
         {
+            if (path == null)
+                return;
+
             var lastDirs = ReadFromFile();
-            if (!lastDirs.Contains(path))
+
+            if (lastDirs?.Count() > 10)
+            {
+                if (IsPath(path))
+                {
+                    lastDirs.ToList().Insert(0, path);
+                    File.AppendAllLines(PathsFile, lastDirs.Take(10));
+                    return;
+                }
+            }
+
+            if (IsPath(path))
             {
                 File.AppendAllText(PathsFile, path + "\n");
             }
         }
+
         public static IEnumerable<string> ReadFromFile()
         {
             if (!File.Exists(PathsFile))
                 return null;
 
-            var lastDirs = File.ReadAllText(PathsFile).Split("\n").Where(x => !(string.IsNullOrEmpty(x)
-                                                                             && string.IsNullOrWhiteSpace(x))).Reverse();
+            var lastDirs = File.ReadAllText(PathsFile)
+                .Split("\n")
+                .Where(x => !(string.IsNullOrEmpty(x) && string.IsNullOrWhiteSpace(x)))
+                .Reverse();
+
             return lastDirs;
+        }
+
+        private static bool IsPath(string path)
+        {
+            return path.Contains("\\");
         }
     }
 }
