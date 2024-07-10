1a176396d855be5b37a77583e478200cc1987540
Thor181 <art.torgaschin@yandex.ru>
2024-07-11 00:03:37 +0700

    feat: up commit of services

--- SvgViewer.V2/App.xaml.cs
+++ SvgViewer.V2/App.xaml.cs
@@ -1,16 +1,24 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using HandyControl.Tools;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SvgViewer.V2.Services;
+using SvgViewer.V2.Utils;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
 
 namespace SvgViewer.V2
 {
     public partial class App : Application
     {
-        public IServiceProvider ServiceProvider { get; set; }
+        public static IServiceProvider ServiceProvider { get; set; }
 
         public App()
+        {
+        }
+
+        static App()
         {
             ServiceProvider = ConfigureServices();
         }
@@ -19,11 +27,21 @@ namespace SvgViewer.V2
         {
             var servicesCollection = new ServiceCollection();
 
-            var config = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            IConfiguration config = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
                                                    .AddJsonFile("_appSettings.json")
                                                    .Build();
 
             servicesCollection.AddSingleton(_ => config);
+            servicesCollection.AddSingleton<VersionService>();
+            servicesCollection.AddSingleton<ClipboardService>();
+
+            servicesCollection.AddKeyedSingleton<LastEntityService>(LastEntityServiceKeys.File, 
+                (x, key) => new LastEntityService(Path.Combine("Configuration", "LastFiles.json")));
+            servicesCollection.AddKeyedSingleton<LastEntityService>(LastEntityServiceKeys.Directory, 
+                (x, key) => new LastEntityService(Path.Combine("Configuration", "LastDirectories.json")));
+
+            servicesCollection.AddSingleton<ImageConverterService>();
+            servicesCollection.AddSingleton<CacheService>();
 
             return servicesCollection.BuildServiceProvider();
         }
