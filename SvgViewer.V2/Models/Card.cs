b28f1df9d120f47e51cac2e5ad8111d322b9cd77
Thor181 <art.torgaschin@yandex.ru>
2024-07-11 00:00:52 +0700

    feat: to CardView (and Card model) added Thumbnail

--- SvgViewer.V2/Models/Card.cs
+++ SvgViewer.V2/Models/Card.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace SvgViewer.V2.Models
 {
@@ -10,11 +13,14 @@ namespace SvgViewer.V2.Models
     {
         public string FilePath { get; set; }
         public string Name { get; set; }
+        public byte[] Thumbnail { get; set; }
+        public bool IsLastFile { get; set; }
 
-        public Card(string path, string name)
+        public Card(string path, string name, byte[] thumbnail)
         {
             FilePath = path;
             Name = name;
+            Thumbnail = thumbnail;
         }
 
         
--- SvgViewer.V2/Views/CardView.xaml.cs
+++ SvgViewer.V2/Views/CardView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using SvgViewer.V2.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,16 +20,14 @@ namespace SvgViewer.V2.Views
     public partial class CardView : UserControl
     {
         public static readonly DependencyProperty CardProperty = DependencyProperty.Register(nameof(Card), typeof(Card), typeof(CardView));
-
-        public Card Card { get => (Card)GetValue(CardProperty); set => SetValue(CardProperty, value); }
-
         public static readonly DependencyProperty ClickCommandProperty = DependencyProperty.Register(nameof(ClickCommand), typeof(ICommand), typeof(CardView));
-
-        public ICommand ClickCommand { get => (ICommand)GetValue(ClickCommandProperty); set => SetValue(ClickCommandProperty, value); }
-
         public static readonly DependencyProperty ClickCommandParameterProperty = DependencyProperty.Register(nameof(ClickCommandParameter), typeof(Card), typeof(CardView));
 
+        public Card Card { get => (Card)GetValue(CardProperty); set => SetValue(CardProperty, value); }
+        public ICommand ClickCommand { get => (ICommand)GetValue(ClickCommandProperty); set => SetValue(ClickCommandProperty, value); }
         public Card ClickCommandParameter { get => (Card)GetValue(ClickCommandParameterProperty); set => SetValue(ClickCommandParameterProperty, value); }
+
+        //public byte[] ThumbnailPath { get => Card.ThumbnailPath; set => Card.ThumbnailPath = value; }
 
         public CardView()
         {
