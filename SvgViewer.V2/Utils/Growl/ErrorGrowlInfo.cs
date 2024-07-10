0040f2ed464b555d6c97a82c278c0d1e42ee7951
Thor181 <art.torgaschin@yandex.ru>
2024-07-11 00:01:13 +0700

    feat: for Growl added static instances

---
+++ SvgViewer.V2/Utils/Growl/ErrorGrowlInfo.cs
@@ -0,0 +1,23 @@
+﻿using HandyControl.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SvgViewer.V2.Utils.Growl
+{
+    public abstract class ErrorGrowlInfo : GrowlInfo
+    {
+        public static readonly GrowlInfo Instance;
+
+        static ErrorGrowlInfo()
+        {
+            Instance = new GrowlInfo()
+            {
+                Message = "Не удалось скопировать",
+                WaitTime = 1
+            };
+        }
+    }
+}
---
+++ SvgViewer.V2/Utils/Growl/SuccessGrowlInfo.cs
@@ -0,0 +1,23 @@
+﻿using HandyControl.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SvgViewer.V2.Utils.Growl
+{
+    public abstract class SuccessGrowlInfo : GrowlInfo
+    {
+        public static readonly GrowlInfo Instance;
+
+        static SuccessGrowlInfo()
+        {
+            Instance = new GrowlInfo()
+            {
+                Message = "Скопировано",
+                WaitTime = 1
+            };
+        }
+    }
+}
