e37e89ab89f098e466818623b6a9490c1611e7b7
Thor181 <art.torgaschin@yandex.ru>
2024-07-11 00:02:10 +0700

    feat: added VersionService

---
+++ SvgViewer.V2/Services/VersionService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SvgViewer.V2.Services
+{
+    public class VersionService
+    {
+        public string? Version { get; set; }
+
+        public VersionService()
+        {
+            var assembly = Assembly.GetEntryAssembly() 
+                ?? throw new InvalidOperationException("Entry assembly can't be null");
+            
+            Version = assembly.GetName().Version?.ToString();
+        }
+    }
+}
