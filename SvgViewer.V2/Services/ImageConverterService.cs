b839f115d19acb9f7354ac40ba6dca6de10dd63b
Thor181 <art.torgaschin@yandex.ru>
2024-07-11 00:02:34 +0700

    feat: added ImageConverterService

---
+++ SvgViewer.V2/Services/ImageConverterService.cs
@@ -0,0 +1,26 @@
+﻿using Svg;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SvgViewer.V2.Services
+{
+    public class ImageConverterService
+    {
+        public byte[] ConvertSvgToPng(string svgPath)
+        {
+            var svg = SvgDocument.Open(svgPath);
+            var bmp = svg.Draw(100, 100);
+
+            using var ms = new MemoryStream();
+
+            bmp.Save(ms, ImageFormat.Png);
+
+            return ms.ToArray();
+        }
+    }
+}
