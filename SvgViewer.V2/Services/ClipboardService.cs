676266ed106251ed376def441a3c1980001f6083
Thor181 <art.torgaschin@yandex.ru>
2024-07-11 00:01:38 +0700

    feat: added ClipboardService

---
+++ SvgViewer.V2/Services/ClipboardService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SvgViewer.V2.Services
+{
+    public class ClipboardService
+    {
+        public void Set(string path)
+        {
+            Clipboard.SetText(path);
+        }
+    }
+}
